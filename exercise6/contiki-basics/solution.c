d92132bd744537b6e01a2a4a2a1d91594eebdc7a
Evgeny Praimovitch <evgenypr@localhost.localdomain>
2010-11-28 17:28:39 +0200

    exercise 6 - stage 8

--- exercise6/contiki-basics/solution.c
+++ exercise6/contiki-basics/solution.c
@@ -8,14 +8,18 @@ PROCESS(hello_world_process, "Hello world process");
 PROCESS(slow_led_blinking_process, "Our leds will be blinking...");
 PROCESS(fast_led_blinking_process, "Red rage...");
 PROCESS(test_button_process, "Test button counter");
-AUTOSTART_PROCESSES(&test_button_process, &hello_world_process, &slow_led_blinking_process);
+PROCESS(custom_event_process, "Custom event receiver");
+AUTOSTART_PROCESSES(&test_button_process, &hello_world_process, &slow_led_blinking_process, &custom_event_process);
 /*---------------------------------------------------------------------------*/
 const static char roller[] = {'/','-','\\','|'};
+static process_event_t customEvent = PROCESS_EVENT_MAX;
+
 PROCESS_THREAD(hello_world_process, ev, data)
 {
   static struct etimer periodicEvent;
   static char stage=0;
   static char blinkingUnleashed = 0;
+  customEvent = process_alloc_event();
   PROCESS_BEGIN();
 
   printf("hello Pavel and Evgeny!\n");
@@ -26,6 +30,7 @@ PROCESS_THREAD(hello_world_process, ev, data)
     if(ev == PROCESS_EVENT_TIMER && (struct etimer*)data == &periodicEvent) {
       printf("%c", roller[stage++%sizeof(roller)]);
       etimer_reset(&periodicEvent);
+      process_post(&custom_event_process, customEvent, NULL);
       if(!blinkingUnleashed) {
 	blinkingUnleashed = 1;
 	process_start(&fast_led_blinking_process, NULL);
@@ -79,5 +84,15 @@ PROCESS_THREAD(test_button_process, ev, data)
     printf("A button has been pressed %d times.\n", ++accidentCounter); 
   }
   
+  PROCESS_END();
+}
+
+PROCESS_THREAD(custom_event_process, ev, data)
+{
+  PROCESS_BEGIN();
+  for(;;) {
+    PROCESS_WAIT_EVENT_UNTIL(ev == customEvent);
+    printf("Custom event registered.\n");
+  }
   PROCESS_END();
 }
