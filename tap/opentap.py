3d0f41a6f7b7375c9fdcf084fcffb8172e2d4a22
Pasha <pasha@pasha-pc.(none)>
2011-02-18 10:58:05 +0200

    Small fix in opentap.py

--- tap/opentap.py
+++ tap/opentap.py
@@ -2,7 +2,7 @@ import ctypes
 
 IFF_NO_PI = 0x1000  # do not provide packet information
 
-lib = ctypes.CDLL("./libopentap.so")
+lib = ctypes.CDLL("libopentap.so")
 
 def opentap(devname):
     fd = lib.opentap(devname, IFF_NO_PI)
