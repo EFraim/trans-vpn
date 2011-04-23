496d80518a7b4e5650139a80ed74dbbd27a83f35
Evgeny Fraimovitch <johnlen7@gmail.com>
2011-04-24 01:41:07 +0300

    Place configuration space where it belongs - reserve the last user-available FLASH sector for configuration.

--- ethtun/appcon.c
+++ ethtun/appcon.c
@@ -3,8 +3,14 @@
 #include "log.h"
 #include "drivers/usb.h"
 #include "usbcon.h"
+#include "vpnConfig.h"
+
+extern int CONFIG_LOCATION;
+extern int CONFIG_SECTOR;
 
 void appcon_loop() {
+  const int SECTOR = (int)&CONFIG_SECTOR;
+  LOG_INFO("Config address is %X, configuration starts at %X, sector %d\n", (int)&CONFIG, (int)&CONFIG_LOCATION, SECTOR);
     for(;;) {
       LOG_DEBUG("I am still alive!\n");
     }
--- ethtun/vpnConfig.h
+++ ethtun/vpnConfig.h
@@ -1,16 +1,18 @@
-static const int MAX_DNS_NAME 255
-static const int KEY_BYTE_LEN 64
+#ifndef VPNCONFIG_H
+#define VPNCONFIG_H
+#include <uip/uip.h>
+
+#define MAX_DNS_NAME 255
+#define KEY_BYTE_LEN 64
 typedef struct {
   enum { DHCP, STATIC } HostileNetAddrConfWay;
   struct {
     uip_ipaddr_t addr, mask, defGateway, dns1, dns2;
-  };
+  } hostileNetStaticConfig;
 
   enum { DNS, HARDWIRED } VpnServerWay;
-  union {
-    char hostName[MAX_DNS_NAME+1];
-    uip_ipaddr_t ipAddr;
-  };
+  char vpnHostName[MAX_DNS_NAME+1];
+  uip_ipaddr_t vpnIpAddr;
 
   unsigned char serverPublicKey[KEY_BYTE_LEN];
   unsigned char clientPrivateKey[KEY_BYTE_LEN];
@@ -18,3 +20,6 @@ typedef struct {
 } vpnConfig;
 
 extern vpnConfig CONFIG;
+
+#endif
+
