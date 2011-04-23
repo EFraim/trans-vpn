079a0593d6300ea942e6b2957f06362a495b4053
evgeny <evgeny@deathstar.(none)>
2011-04-23 23:49:15 +0300

    Merging of config infrastructure

---
+++ eth/vpnConfig.c
@@ -0,0 +1,5 @@
+#include "vpnConfig.h"
+
+//This should NOT be initialized. It is a configuration which is not overwritten by firmware update. Thus it should be kept in bss section;
+
+vpnConfig CONFIG;
